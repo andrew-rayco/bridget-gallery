1d07e8c9bcbe984db4e5723764026db615ffa8c0
andrew-rayco <andy@andycondon.com>
2017-03-10 18:37:42 +1300

    add lightbox to catalogues page

--- js/site.js
+++ js/site.js
@@ -2,7 +2,7 @@ $(function() {
     var $menu = $('#menu');
     var $menuIcon = $('#nav-icon')
     var $navItems = $('#menu li')
-    
+
     var $menuShowHide = function() {
         if ($(window).width() < 767) {
             $menu.hide();
@@ -10,16 +10,23 @@ $(function() {
             $menu.show();
         };
     };
-    
+
     $menuShowHide();
-    
+
     $(window).resize(function() {
         $menuShowHide();
     });
-    
+
     $menuIcon.on('click', function() {
         $menu.slideToggle(200);
         $navItems.hide().fadeIn(1000);
     });
-    
+
+});
+
+
+$('.fl').on('click', function() {
+  var $content = '<h2>Artist name</h2>';
+  $content += $(this).html();
+  $.featherlight($content);
 });
